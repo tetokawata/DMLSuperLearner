a15f365b943b6a6df79255a1c04c5fb090691b0f
川田恵介 <keisukekawata@iss.u-tokyo.ac.jp>
2021-12-06 21:54:07 +0900

    Start

---
+++ R/AIPW_SLearner.R
@@ -0,0 +1,121 @@
+#' @title AIPW with Super Learner
+#'
+#' @description \code{AIPW_SLearner} is used to implement AIPW with Super Learner. Current version trim sample with propensity <= 0.05 or >= 0.95
+#'
+#' @param Y Outcome vector
+#' @param D Treatment vector (Binary)
+#' @param X Control as data frame
+#' @param SL.DML.library SL.library to estimate nuisance functions. Use first method if all SL coefficient are zero.
+#' @param DML.V Number of folds
+#' @param SL.V Number of folds in each SuperLearner
+#' @param weights Sample weights
+#' @param D.family gaussian() or binomial() to describe the error distribution.
+#' @param p.score propensity score vector
+#'
+#' @return A list containing follows:
+#' \item{data}{ Data frame including estimated nuisance}
+#' \item{parameter_estimation}{ Estimation result}
+#'
+#' @export
+#'
+#' @examples
+#'
+#' # Example data
+#' X <- matrix(rnorm(200,0,1),100)
+#' D <- 2*X[,1] + runif(100,-10,10)
+#' Y <- 3*X[,1] + rnorm(100,0,1)
+#'
+#' # Estimation
+#' fit <- AIPW_SLearner(X = X, D = D, Y = Y)
+#' fit$parameter_estimation
+#'
+#' @references
+#' Robins, J. M., Rotnitzky, A., & Zhao, L. P. (1994). Estimation of regression coefficients when some regressors are not always observed. Journal of the American statistical Association, 89(427), 846-866.
+#'
+#' van der Laan, M. J., Polley, E. C. and Hubbard, A. E. (2007) Super Learner. Statistical Applications of Genetics and Molecular Biology, 6, article 25.
+#'
+#' Chernozhukov, V., Chetverikov, D., Demirer, M., Duflo, E., Hansen, C., & Newey, W. (2017). Double/debiased/neyman machine learning of treatment effects. American Economic Review, 107(5), 261-65.
+#'
+
+AIPW_SLearner <- function(X,
+                          D,
+                          Y,
+                          SL.DML.library = c("SL.ranger","SL.mean"),
+                          DML.V = 2,
+                          SL.V = 2,
+                          core  = 1,
+                          process = TRUE,
+                          weights = NULL,
+                          D.family = binomial(),
+                          p.score = NULL){
+  require(magrittr)
+  require(furrr)
+  require(SuperLearner)
+  # weight
+  if(is.null(weights)) {
+    weights <- rep(1, length(Y))
+  }
+  # Grouping
+  DML.group <- sample(1:DML.V, length(Y), replace = TRUE)
+  # Each nuisance
+  eachDML <- function(g){
+    # Y1
+    temp = SuperLearner(X = X[DML.group != g & D == 1,],
+                                      Y = Y[DML.group != g & D == 1],
+                                      newX = X[DML.group == g,],
+                                      SL.library = SL.DML.library,
+                                      cvControl = SuperLearner.CV.control(V = SL.V)
+                                      )
+    Y1.hat = temp$library.predict[,1]
+    if (is_empty(temp$SL.predict) == FALSE) {Y1.hat = temp$SL.predict}
+    # Y0
+    temp = SuperLearner(X = X[DML.group != g & D == 0,],
+                                      Y = Y[DML.group != g & D == 0],
+                                      newX = X[DML.group == g,],
+                                      SL.library = SL.DML.library,
+                                      cvControl = SuperLearner.CV.control(V = SL.V)
+                                      )
+    Y0.hat = temp$library.predict[,1]
+    if (is_empty(temp$SL.predict) == FALSE) {Y0.hat = temp$SL.predict}
+    # D
+    D.hat = p.score[DML.group == g]
+    if (is.null(p.score)) {temp = SuperLearner(X = X[DML.group != g,],
+                                               Y = D[DML.group != g],
+                                               newX = X[DML.group == g,],
+                                               SL.library = SL.DML.library,
+                                               cvControl = SuperLearner.CV.control(V = SL.V),
+                                               family = D.family
+                                               )
+    D.hat = temp$library.predict[,1]
+    if (is_empty(temp$SL.predict) == FALSE) {D.hat = temp$SL.predict}}
+    tibble::tibble(Y = Y[DML.group == g],
+                   D = D[DML.group == g],
+                   Y1.hat = Y1.hat,
+                   Y0.hat = Y0.hat,
+                   D.hat = D.hat,
+                   X[DML.group == g,],
+                   weights = weights[DML.group == g])
+  }
+  # Iteration
+  plan(multisession, workers = core)
+  result <- list(data = future_map_dfr(1:DML.V,
+                                       eachDML,
+                                       .options = furrr_options(seed = 1L),
+                                       .progress = process)
+  )
+  # Store results
+  result$parameter_estimation <-
+    result$data |>
+    dplyr::filter(D.hat >= 0.05 & D.hat <= 0.95) |>
+    dplyr::mutate(Y1.potential = Y1.hat + (D/D.hat)*(Y - Y1.hat),
+           Y0.potential = Y0.hat + ((1-D)/(1-D.hat))*(Y - Y0.hat),
+           aipw = Y1.potential - Y0.potential) %>%
+    estimatr::lm_robust(aipw ~ 1,
+              data = .,
+              weights = weights) |>
+    generics::tidy() |>
+    dplyr::mutate(outcome = "ATE",
+           term = "AIPW")
+  result
+}
+
---
+++ R/Robinson_SLearner.R
@@ -0,0 +1,100 @@
+#' @title Robinson estimation with Super Learner
+#'
+#' @description \code{Robinson_SLearner} is used to implement Robinson estimation with Super Learner
+#'
+#' @param Y Outcome vector
+#' @param D Treatment vector
+#' @param X Control as data frame
+#' @param SL.DML.library SL.library to estimate nuisance functions. Use first method if all SL coefficient are zero.
+#' @param DML.V Number of folds
+#' @param SL.V Number of folds in each SuperLearner
+#' @param weights Sample weights
+#'
+#' @return A list containing follows:
+#' \item{data}{ Data frame including estimated nuisance}
+#' \item{parameter_estimation}{ Estimation result}
+#'
+#' @export
+#'
+#' @examples
+#'
+#' # Example data
+#' X <- matrix(rnorm(200,0,1),100)
+#' D <- 2*X[,1] + runif(100,-10,10)
+#' Y <- 3*X[,1] + rnorm(100,0,1)
+#'
+#' # Estimation
+#' fit <- Robinson_SLearner(X = X, D = D, Y = Y)
+#' fit$parameter_estimation
+#'
+#' @references
+#' Robinson, P. M. (1988). Root-N-consistent semiparametric regression. Econometrica, 931-954.
+#'
+#' Chernozhukov, V., Chetverikov, D., Demirer, M., Duflo, E., Hansen, C., Newey, W., & Robins, J. (2018). Double/debiased machine learning for treatment and structural parameters.
+#'
+#' van der Laan, M. J., Polley, E. C. and Hubbard, A. E. (2007) Super Learner. Statistical Applications of Genetics and Molecular Biology, 6, article 25.
+#'
+
+
+Robinson_SLearner <- function(X,
+                      D,
+                      Y,
+                      SL.DML.library = c("SL.ranger","SL.mean"),
+                      DML.V = 2,
+                      SL.V = 2,
+                      core  = 1,
+                      process = TRUE,
+                      weights = NULL){
+  require(magrittr)
+  require(furrr)
+  require(SuperLearner)
+  # Weight
+  if(is.null(weights)) {
+    weights <- rep(1, length(Y))
+  }
+  # Data split
+  DML.group <- sample(1:DML.V, length(Y), replace = TRUE)
+  # Estimate nuisance
+  eachDML <- function(g){
+    temp = SuperLearner(X = X[DML.group != g,],
+                                      Y = Y[DML.group != g],
+                                      newX = X[DML.group == g,],
+                                      SL.library = SL.DML.library,
+                                      cvControl = SuperLearner.CV.control(V = SL.V))
+    Y.hat = temp$library.predict[,1]
+    if (is_empty(temp$SL.predict) == FALSE) {Y.hat = temp$SL.predict}
+    temp = SuperLearner(X = X[DML.group != g,],
+                                      Y = D[DML.group != g],
+                                      newX = X[DML.group == g,],
+                                      SL.library = SL.DML.library,
+                                      cvControl = SuperLearner.CV.control(V = SL.V))
+    D.hat = temp$library.predict[,1]
+    if (is_empty(temp$SL.predict) == FALSE) {D.hat = temp$SL.predict}
+    tibble::tibble(Y = Y[DML.group == g],
+                   D = D[DML.group == g],
+                   Y.hat = Y.hat,
+                   D.hat = D.hat,
+                   X[DML.group == g,],
+                   weights = weights[DML.group == g])
+  }
+  # Iteration
+  plan(multisession, workers = core)
+  result <- list(data = future_map_dfr(1:DML.V,
+                 eachDML,
+                 .options = furrr_options(seed = 1L),
+                 .progress = process)
+                 )
+  # Estimate R-learner
+  result$parameter_estimation <-
+    result$data |>
+    dplyr::mutate(Y.oht = Y - Y.hat,
+                  D.oht = D - D.hat) %>%
+    estimatr::lm_robust(Y.oht ~ 0 + D.oht,
+              data = .,
+              weights = weights) |>
+    generics::tidy() |>
+    dplyr::mutate(outcome = "Y",
+           term = "Rlearner")
+  result
+}
+
